79e595c6e803c0027f79a459d99698f9603d9789
LucasHBarth <contato@barthdesignn.com>
2022-05-20 17:17:16 -0300

    V6

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,10 +37,19 @@ let swiperPopular = new Swiper(".popular__container", {
     loop: true,
     spaceBetween: 24,
     slidesPerView: 'auto',
+    grabCursor: true,
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 48,
+        },
+    }
 });
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
